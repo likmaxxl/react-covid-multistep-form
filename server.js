87bc7f6c05b1d21c7ce3760d3b6d127f69696e92
Malik <malikalkhafaji@yahoo.com>
2022-04-19 21:23:24 +0200

    adding json server

--- server.js
+++ server.js
@@ -1,32 +1,6 @@
 const jsonServer = require('json-server');
-const cors = require("cors")
 const server = jsonServer.create();
 const router = jsonServer.router('./data/db.json');
-
-
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config()
-}
-
-const domainsFromEnv = process.env.CORS_DOMAINS || ""
-const whitelist = domainsFromEnv.split(",").map(item => item.trim())
-const whitelist = ["http://localhost:8000"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
-
-
-
-
 
 
 const middlewares = jsonServer.defaults({
